ef7a1adbad7b5e6c5de34a5c82159bc3e8e94a95
Brian Su <briansu2004@hotmail.com>
2024-01-10 21:06:28 -0500

    $

---
+++ build.bat
@@ -0,0 +1,7 @@
+@cls
+
+npm run build
+
+
+@REM npm install -g serve
+@REM serve -s build
--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import CoursesList from './components/CoursesList';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// <div><h1>My Udemy Courses</h1></div>
 function App() {
   return (
     <Router>
       <div className="app">
-        <div><h1>My Udemy Courses</h1></div>
+        
         <Route path="/" exact component={CoursesList} />
       </div>
     </Router>
